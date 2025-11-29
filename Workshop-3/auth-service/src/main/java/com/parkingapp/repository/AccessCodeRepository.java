d21f268e99534378d397752fde943064fb7bfd03
lopezander <ander.jeffrey@gmail.com>
2025-11-29 16:52:09 -0500

    Reorganization of folders for proper delivery of the final project

--- Workshop-3/auth-service/src/main/java/com/parkingapp/repository/AccessCodeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.parkingapp.repository;
-
-import com.parkingapp.model.AccessCode;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface AccessCodeRepository extends JpaRepository<AccessCode, Long> {
-    Optional<AccessCode> findByCode(String code);
-}
--- Workshop-3/auth-service/src/main/java/com/parkingapp/service/AuthService.java
+++
@@ -1,9 +0,0 @@
-package com.parkingapp.service;
-
-import com.parkingapp.dto.LoginRequest;
-import com.parkingapp.dto.RegisterRequest;
-
-public interface AuthService {
-    void register(RegisterRequest request);
-    String login(LoginRequest request);
-}
