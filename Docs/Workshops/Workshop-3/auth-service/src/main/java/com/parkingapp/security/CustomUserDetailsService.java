3fb0d495c9106286de17686637cdf493c1640b56
lopezander <ander.jeffrey@gmail.com>
2025-11-29 18:10:21 -0500

    Updated registration form and API request to remove access code field

---
+++ Docs/Workshops/Workshop-3/auth-service/src/main/java/com/parkingapp/security/CustomUserDetailsService.java
@@ -0,0 +1,41 @@
+package com.parkingapp.security;
+
+import com.parkingapp.model.User;
+import com.parkingapp.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("Email no encontrado"));
+
+        List<SimpleGrantedAuthority> authorities = user.getRoles().stream()
+        .map(r -> new SimpleGrantedAuthority(r.getName()))
+        .collect(Collectors.toList());
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                user.isEnabled(),
+                true,
+                true,
+                true,
+                authorities
+        );
+    }
+}
